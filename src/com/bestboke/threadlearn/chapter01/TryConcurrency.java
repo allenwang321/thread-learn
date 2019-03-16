a5cae6ea1113fa22371eed18a543648ea619d6b4
allenwang321 <zbxhui@gmail.com>
2019-03-16 13:46:34 +0800

    尝试让两个同时运行

---
+++ src/com/bestboke/threadlearn/chapter01/TryConcurrency.java
@@ -0,0 +1,37 @@
+package com.bestboke.threadlearn.chapter01;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class TryConcurrency {
+
+    public static void main(String [] args){
+        browseNews();
+        enjoyMusic();
+
+    }
+
+    private static void browseNews(){
+        for (;;){
+            System.out.println("Uh-huh,the good news.");
+            sleep(1);
+        }
+    }
+
+    private static void enjoyMusic(){
+        for (;;){
+            System.out.println("Uh-huh,the good music.");
+            sleep(1);
+        }
+    }
+
+    private static void sleep(int seconds){
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
