2004f89c4dac41977520f57d1be8d46ecc7f89eb
allenwang321 <zbxhui@gmail.com>
2019-03-16 14:02:32 +0800

    新增线程，以及README

---
+++ src/com/bestboke/threadlearn/chapter01/README.md
@@ -0,0 +1,5 @@
+##### TryConcurrency
+尝试同时运行两个方法，一个代表听音乐一个代表看新闻，但是并没有实现最初的想法
+
+##### TryConcurrencyUpdate
+借助Thread实现以上的想法，一边听音乐一般看新闻
---
+++ src/com/bestboke/threadlearn/chapter01/TryConcurrencyUpdate.java
@@ -0,0 +1,41 @@
+package com.bestboke.threadlearn.chapter01;
+
+import java.util.concurrent.TimeUnit;
+
+public class TryConcurrencyUpdate {
+
+    public static void main(String [] args){
+
+        new Thread(){
+            @Override
+            public void run() {
+                browseNews();
+            }
+        }.start();
+
+        enjoyMusic();
+
+    }
+
+    private static void browseNews(){
+        for (;;){
+            System.out.println("Uh-huh,the good news.");
+            sleep(1);
+        }
+    }
+
+    private static void enjoyMusic(){
+        for (;;){
+            System.out.println("Uh-huh,the good music.");
+            sleep(1);
+        }
+    }
+
+    private static void sleep(int seconds){
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
